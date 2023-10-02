ee3f26849ad5a8f050e1f1da65deacc5ce22c8e6
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-10-02 19:40:24 -0300

    feat: Camada de aplicação (Serviço)

---
+++ src/main/java/com/challenge/emailservice/application/EmailSenderService.java
@@ -0,0 +1,22 @@
+package com.challenge.emailservice.application;
+
+import org.springframework.stereotype.Service;
+
+import com.challenge.emailservice.adapters.EmailSenderGateway;
+import com.challenge.emailservice.core.EmailSenderUseCase;
+
+@Service
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    public EmailSenderService(EmailSenderGateway emailSenderGateway) {
+        this.emailSenderGateway = emailSenderGateway;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+    }
+    
+}
