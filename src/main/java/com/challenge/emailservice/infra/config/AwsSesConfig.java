cb9271e8d75b7e898f778b3acbd8739faad3d18f
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-10-02 21:57:35 -0300

    fix: Correção do erro "Unable to load AWS credentials"

--- src/main/java/com/challenge/emailservice/infra/config/AwsSesConfig.java
+++ src/main/java/com/challenge/emailservice/infra/config/AwsSesConfig.java
@@ -1,4 +1,4 @@
-package com.challenge.emailservice.infra.ses;
+package com.challenge.emailservice.infra.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +11,10 @@ public class AwsSesConfig {
     
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService() {
-        return AmazonSimpleEmailServiceClientBuilder.standard().build();
+        return AmazonSimpleEmailServiceClientBuilder
+            .standard()
+            .withCredentials(new MyAWSCredentialsProvider("YOUR_AWS_ACCESS_KEY_ID", "YOUR_AWS_SECRET_ACCESS_KEY"))
+            .withRegion("sa-east-1")
+            .build();
     }
 }
---
+++ src/main/java/com/challenge/emailservice/infra/config/MyAWSCredentialsProvider.java
@@ -0,0 +1,27 @@
+package com.challenge.emailservice.infra.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+
+public class MyAWSCredentialsProvider implements AWSCredentialsProvider {
+
+    private String accessKeyId;
+    private String secretAccessKey;
+
+    public MyAWSCredentialsProvider(String accessKeyId, String secretAccessKey) {
+        this.accessKeyId = accessKeyId;
+        this.secretAccessKey = secretAccessKey;
+    }
+
+    @Override
+    public AWSCredentials getCredentials() {
+        return new BasicAWSCredentials(accessKeyId, secretAccessKey);
+    }
+
+    @Override
+    public void refresh() {
+        //Atualizar credenciais AWS
+    }
+    
+}
